e05abd637a669975ac0bae95938a8b0a0dff4784
ei17ringo <ei17ringo@gmail.com>
2015-01-14 15:43:21 +0800

    pushで画面遷移するcoffee辞典

--- SampleTableView002/ViewController.h
+++ SampleTableView002/ViewController.h
@@ -8,7 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDelegate,UITableViewDataSource>
+{
+    NSArray *_coffeeArray; //メンバ変数宣言
+}
+
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
 
 
 @end
